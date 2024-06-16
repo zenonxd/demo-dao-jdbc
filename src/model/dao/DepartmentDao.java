c6fd89cd6d4598212ab3866c57794ca5612bd4b4
zenonxd <olavomoreira99@gmail.com>
2024-06-16 19:21:23 -0300

    insert implementation

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -5,7 +5,7 @@ import model.entities.Department;
 import java.util.List;
 
 public interface DepartmentDao {
-    void insert(Department obg);
+    void insert(Department obj);
     void update(Department obj);
     void deleteById(Integer id);
     Department findById(Integer id);
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -6,8 +6,8 @@ import model.entities.Seller;
 import java.util.List;
 
 public interface SellerDao {
-    void insert(Department obg);
-    void update(Department obj);
+    void insert(Seller obj);
+    void update(Seller obj);
     void deleteById(Integer id);
     Seller findById(Integer id);
     List<Seller> findAll();
